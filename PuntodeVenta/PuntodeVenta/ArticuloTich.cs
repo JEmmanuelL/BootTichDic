265cfced513a1c6dbb427ca8735bf3df3d16bb80
JEmmanuelL <Emma_lopez2917@hotmail.com>
2024-11-29 10:56:29 -0600

    Creación del proyecto Punto de Venta
    Se agrega las clases principales del proyecto

---
+++ PuntodeVenta/PuntodeVenta/ArticuloTich.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuntodeVenta
+{
+    internal class ArticuloTich : IArticulo
+    {
+        public string imprimir()
+        {
+            return "Articulo de tich";
+        }
+    }
+}
